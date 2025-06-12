6f1a43baa5f5a080e3e7249afc69e114c393e78a
pglen <peterglen99@gmail.com>
2025-06-12 16:31:25 +0700

    autocheck

--- pack.sh
+++ pack.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
-# Pack current project. Will back out from dir and create dirname.tgz
-# and put it back to our dir.
+# Pack current project. Will back out from dir and create
+# dirname-version.tgz
 
 VER=`cat VERSION`
 CURR=`pwd | awk -F '/' {'print $NF'} |  sort -n | tail -1`
 #echo $CURR-$VER
+
+if [ -f ../$CURR-$VER.tgz ] ; then
+    echo "Already exists: ../$CURR-$VER.tgz ; Please delete first."
+    exit 1
+fi
+
 SSS=`pwd`
 cd ..
 echo "Packing project $CURR-$VER"
 tar cfz $CURR-$VER.tgz $CURR
 cd $SSS
-mv ../$CURR-$VER.tgz .
+#mv ../$CURR-$VER.tgz .
 
 
 
